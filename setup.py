2ce67111ce26a4f316ef55177769e5da0dc86608
jimmyzhang610 <cgzhang6@iflytek.com>
2023-09-14 15:41:38 +0800

    update dependents

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ setuptools.setup(
         "transformers>=4.1",
         "zstandard",
         "accelerate>=0.17.1",
+        "rouge_chinese",
+        "jieba",
     ],
     extras_require={
         "dev": ["black", "flake8", "pre-commit", "pytest", "pytest-cov"],
